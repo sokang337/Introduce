a5d1ee149dddb6d7e483f2a7050520bd89c9fa6f
ssilook <ssilook@mz.co.kr>
2022-07-12 17:03:17 +0900

    update vue.config

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,16 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
     transpileDependencies: true,
     outputDir: './docs',
-    lintOnSave: false
+    lintOnSave: false,
+    css: {
+        loaderOptions: {
+            sass: {
+                sassOptions: {
+                    includePaths: ['./node_modules', './src/assets']
+                }
+            }
+        }
+    }
     /* css: {
         loaderOptions: {
             sass: {
